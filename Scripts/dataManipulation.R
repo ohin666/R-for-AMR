51b9ee4cb82adebe2aaada1c99f5396144fb22ae
Owen <owen.6@live.com>
2024-09-17 15:40:23 +0100

    extra manipukatio

--- Scripts/dataManipulation.R
+++ Scripts/dataManipulation.R
@@ -32,3 +32,45 @@ Africa = gapminder %>%
 gapminder %>%
   filter(continent == 'Africa') %>%
   select(lifeExp, country, year) 
+
+
+
+gapminder %>% 
+  group_by(continent) %>%
+  summarise(count = n(),
+    mean_GDP = mean(gdpPercap),
+    minGDP = min(gdpPercap))
+
+# summarise by year
+gapminder %>% 
+  group_by(year) %>%
+  summarise(count = n(),
+            mean_GDP = mean(gdpPercap),
+            minGDP = min(gdpPercap))
+
+
+
+
+meanLifeExp = gapminder %>% 
+  group_by(country) %>%
+  summarise(count = n(),
+            meanLife = mean(lifeExp),
+            maxLife = min(lifeExp),
+            )
+
+
+meanLifeExp %>%
+  filter(meanLife == min(meanLife) | meanLife == max(meanLife))
+
+
+
+
+gapminder %>% 
+  group_by(country, year) %>%
+  summarise(count = n(),
+    mean_gdp = mean(gdpPercap),
+            sd_gdp = stats::sd(gdpPercap),
+            mean_pop = mean(pop),
+            sd_pop = stats::sd(pop))
+ 
+
