44c458b2996392a1da22efbd49111ac23e1618e1
Owen <owen.6@live.com>
2024-09-17 16:50:14 +0100

    new script advancedPlot.R 2

--- Scripts/advancedplot.R
+++ Scripts/advancedplot.R
@@ -4,3 +4,29 @@ require(tidyverse)
 
 gapminder = read.csv('data/gapminder_data.csv')
 
+# select America data
+gapminder %>%
+  filter(continent == 'Americas') %>%
+  ggplot(aes(year, lifeExp)) +
+  geom_line() +
+  facet_wrap(~ country) +
+  theme(axis.text.x = element_text(angle = 45))
+   
+gapminder %>%
+  mutate(startsWith = substr(country,1,1)) %>%
+  filter(startsWith %in% c('A', 'Z')) %>%
+  ggplot(aes(year, lifeExp, colour = continent)) +
+  geom_line() +
+  facet_wrap(~ country) +
+  theme(axis.text.x = element_text(angle = 45))
+  # theme_minimal()
+
+
+
+gapminder %>%
+  filter(year == 2002) %>%
+  group_by(continent, country) %>%
+  summarise(mean_lifeExp = mean(lifeExp)) %>%
+  sample_n(size = 2) 
+  
+  
