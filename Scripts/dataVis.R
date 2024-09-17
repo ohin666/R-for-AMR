7eb58e8819f09b5ff56b92a2a847294b6718a2be
Owen <owen.6@live.com>
2024-09-17 12:25:13 +0100

    scatterplot

--- Scripts/dataVis.R
+++ Scripts/dataVis.R
@@ -5,4 +5,9 @@ require(rms)
 
 gapminder <- read.csv('data/gapminder_data.csv')
 
-
+# plot GDP vs per capita
+gapminder %>%
+  # filter(year == 1962) %>%
+  ggplot() +
+  geom_point(aes(gdpPercap, lifeExp))
+  
